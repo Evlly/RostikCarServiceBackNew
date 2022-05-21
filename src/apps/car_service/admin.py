eb21b6adf5f2b45bbbb57f88ca6add4e6cadc972
Anton Abramov <antonabramov2000@gmail.com>
2022-05-21 15:39:04 +0700

    Update admin.py

--- src/apps/car_service/admin.py
+++ src/apps/car_service/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.car_service.models import TypeService, Service, Order, OrderStatus
+from apps.car_service.models import TypeService, Service, Order, OrderStatus, Detail
 
 
 @admin.register(Service)
